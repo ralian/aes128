2c97e9620557c9ecf25d59f8a3fa833f7030208e
William Bowers <will@debtop.ralian.net>
2018-11-01 10:41:28 -0500

    Add CMake build script

---
+++ aes.hpp
@@ -0,0 +1,15 @@
+// Author: Will Bowers (willb0102@gmail.com)
+// Date:   31/10/18
+
+#pragma once
+
+#include <tuple>
+
+using std::tuple;
+
+// The a128 type is used to transmit and store 128 bit keys and ciphers
+typedef std::tuple<uint64_t, uint64_t> a128;
+
+a128 e(a128 k, a128 x);
+
+a128 d(a128 k, a128 y);
