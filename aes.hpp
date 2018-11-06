245c6e38426b2f2de769659b50adcd189a5b0cab
William Bowers <will@debtop.ralian.net>
2018-11-06 10:50:18 -0600

    Add row and column operations to the round

--- aes.hpp
+++ aes.hpp
@@ -10,6 +10,14 @@ using std::get;
 using std::uint64_t;
 using std::uint32_t;
 
+// block is used to break down a 128 bit key into 32 bit rows
+// and 8 bit blocks.
+union block {
+	uint64_t pair[2];
+	uint32_t row[4];
+	uint8_t byte[16];
+};
+
 // The a128 type is used to transmit and store 128 bit keys and ciphers
 typedef std::tuple<uint64_t, uint64_t> a128;
 
